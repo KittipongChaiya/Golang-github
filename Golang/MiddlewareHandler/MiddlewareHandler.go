a47e9152a5dee6ddcce2cf0dc18e2c8558a07998
KittipongChaiya <111034922+KittipongChaiya@users.noreply.github.com>
2024-11-14 16:05:51 +0700

    Create MiddlewareHandler.go

---
+++ Golang/MiddlewareHandler/MiddlewareHandler.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type Course struct {
+	ID         int     `json:"id"`
+	Name       string  `json:"name"`
+	Price      float64 `json:"price"`
+	Instructor string  `json:"instructor"`
+}
+
+var CourseList []Course
+
+func init() {
+	CourseJSON := `[
+		{
+			"id":1,
+			"name":"Python",
+			"price":2500,
+			"instructor":"Rom"
+		},
+		{
+			"id":2,
+			"name":"Angular",
+			"price":1000,
+			"instructor":"Rom"
+		},
+		{
+			"id":3,
+			"name":"Net",
+			"price":5000,
+			"instructor":"Rom"
+		}
+	]`
+	err := json.Unmarshal([]byte(CourseJSON), &CourseList)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getNextID() int {
+	highestID := -1
+	for _, course := range CourseList {
+		if highestID < course.ID {
+			highestID = course.ID
+		}
+	}
+	return highestID + 1
+}
+
+func FindID(ID int) (*Course, int) {
+	for i, course := range CourseList {
+		if course.ID == ID {
+			return &course, i
+		}
+	}
+	return nil, 0
+}
+
+func courseHandler(w http.ResponseWriter, r *http.Request) {
+	urlPathSegment := strings.Split(r.URL.Path, "course/")
+	ID, err := strconv.Atoi(urlPathSegment[len(urlPathSegment)-1])
+	if err != nil {
+		log.Print(err)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	course, listItemIndex := FindID(ID)
+	if course == nil {
+		http.Error(w, fmt.Sprintf("No course with id %d", ID), http.StatusNotFound)
+		return
+	}
+	switch r.Method {
+	case http.MethodGet:
+		courseJSON, err := json.Marshal(course)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(courseJSON)
+
+	case http.MethodPut:
+		var updateCourse Course
+		bytesBody, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = json.Unmarshal(bytesBody, &updateCourse)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if updateCourse.ID != ID {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		course = &updateCourse
+		CourseList[listItemIndex] = *course
+		w.WriteHeader(http.StatusOK)
+		return
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+
+}
+
+func coursesHandler(w http.ResponseWriter, r *http.Request) {
+	courseJSON, err := json.Marshal(CourseList)
+	switch r.Method {
+
+	case http.MethodGet:
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(courseJSON)
+
+	case http.MethodPost:
+
+		var newCourse Course
+
+		Bodybyte, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err = json.Unmarshal(Bodybyte, &newCourse)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if newCourse.ID != 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		newCourse.ID = getNextID()
+		CourseList = append(CourseList, newCourse)
+		w.WriteHeader(http.StatusCreated)
+		return
+
+	}
+}
+
+func middlewareHandler(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("before handler middle start")
+
+		handler.ServeHTTP(w, r)
+		fmt.Println("middleware finised")
+	})
+}
+
+func main() {
+
+	courseItemHandler := http.HandlerFunc(courseHandler)
+	courseListHandler := http.HandlerFunc(coursesHandler)
+
+	http.Handle("/course/", middlewareHandler(courseItemHandler))
+	http.Handle("/course", middlewareHandler(courseListHandler))
+	http.ListenAndServe(":5000", nil)
+}
